c1c52bb9415a19980edd22e6b92149892a6af86f
Daniel Barenholz <hello@dbarenholz.com>
2023-12-04 12:13:18 +0100

    Day 1, Part 2.hs

--- src/one/one.hs
+++ src/one/one.hs
@@ -1,4 +1,5 @@
 import Data.Char (isDigit)
+import Data.List (isPrefixOf, tails)
 
 main :: IO ()
 main = interact (formatOutput . solve . parseInput)
@@ -13,6 +14,22 @@ formatOutput solution = show solution ++ "\n"
 parseInput :: String -> Input
 parseInput = lines
 
+innerProcess :: String -> Char
+innerProcess s
+  | "one" `isPrefixOf` s = '1'
+  | "two" `isPrefixOf` s = '2'
+  | "three" `isPrefixOf` s = '3'
+  | "four" `isPrefixOf` s = '4'
+  | "five" `isPrefixOf` s = '5'
+  | "six" `isPrefixOf` s = '6'
+  | "seven" `isPrefixOf` s = '7'
+  | "eight" `isPrefixOf` s = '8'
+  | "nine" `isPrefixOf` s = '9'
+  | otherwise = head s
+
+preprocess :: String -> String
+preprocess s = map innerProcess (init (tails s))
+
 duplicateSingleCharacter :: String -> String
 duplicateSingleCharacter [x] = [x, x]
 duplicateSingleCharacter s = s
@@ -21,4 +38,4 @@ toFirstAndLastDigit :: String -> String
 toFirstAndLastDigit s = [head s, last s]
 
 solve :: Input -> Output
-solve s = sum (map (read . toFirstAndLastDigit . duplicateSingleCharacter . filter isDigit) s)
+solve s = sum (map (read . toFirstAndLastDigit . duplicateSingleCharacter . filter isDigit . preprocess)  s)
